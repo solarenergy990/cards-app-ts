45dc013836ec8b91cd7ab8aeaa59ac978b3e7638
Eugen Soliar <eugen.soliar@exxeta.com>
2022-07-06 13:46:35 +0200

    erases useless comments

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -5,152 +5,24 @@ import ApprovedList from './ApprovedList/ApprovedList';
 import Modal from '../Modal/Modal';
 import ApplicantForm from '../ApplicantForm/ApplicantForm';
 
-// import shortid from 'shortid';
-
-// import initialApplicants from '../../components/Dashboard/applicants.json';
-
-// import IApplicant from '../../interfaces/IApplicant.inteface';
-import {  useAppSelector } from '../../redux/hooks/hooks';
-
-
+import { useAppSelector } from '../../redux/hooks/hooks';
 
 const Dashboard = () => {
-  // const [applicants, setApplicants] = useState<IApplicant[]>(initialApplicants);
   const [modalActive, setModalActive] = useState(false);
 
-  const applicants = useAppSelector(({applicants}) => {
+  const applicants = useAppSelector(({ applicants }) => {
     return applicants;
   });
-
-  // const addApplicant = (data: IApplicant) => {
-  //   const { name, number, desiredPosition, status } = data;
-
-  //   const applicant = {
-  //     id: shortid.generate(),
-  //     name,
-  //     number,
-  //     desiredPosition,
-  //     status,
-  //   };
-
-  //   const checkedApplicantsNames = applicants.map(applicant => {
-  //     return applicant.name.toLowerCase();
-  //   });
-
-  //   if (!checkedApplicantsNames.includes(name.toLowerCase())) {
-  //     setApplicants(prevApplicants => {
-  //       return [...prevApplicants, applicant];
-  //     });
-  //   } else {
-  //     alert(`${name} is already in list`);
-  //   }
-  // };
-
-  // const deleteApplicant = (applicantId: string) => {
-  //   setApplicants(applicants.filter(applicant => applicant.id !== applicantId));
-  // };
-
-  // const makeApplicantAppointment = (applicantId: string) => {
-  //   const applicantForInterview = applicants.find(
-  //     applicant => applicantId === applicant.id,
-  //   );
-
-  //   if (applicantForInterview) {
-  //     const { id, name, number, desiredPosition } = applicantForInterview;
-
-  //     const applicant = {
-  //       id,
-  //       name,
-  //       number,
-  //       desiredPosition,
-  //       status: 'interview',
-  //     };
-
-  //     // erasing an applicant from previous status line
-  //     setApplicants(
-  //       applicants.filter(applicant => applicant.id !== applicantId),
-  //     );
-
-  //     // setting new array of applicants with changed applicant
-  //     setApplicants(prevApplicants => {
-  //       // console.log(prevApplicants)
-  //       return [...prevApplicants, applicant];
-  //     });
-  //   }
-
-  //   // temporary
-  //   // if (applicantForInterview) {
-  //   //   applicantForInterview.status = 'interview';
-  //   // }
-  // };
-
-  // const approveApplicant = (applicantId: string) => {
-  //   const applicantForApproval = applicants.find(
-  //     applicant => applicantId === applicant.id,
-  //   );
-
-  //   if (applicantForApproval) {
-  //     const { id, name, number, desiredPosition } = applicantForApproval;
-
-  //     const applicant = {
-  //       id,
-  //       name,
-  //       number,
-  //       desiredPosition,
-  //       status: 'approved',
-  //     };
-
-  //     // erasing an applicant from previous status line
-  //     setApplicants(
-  //       applicants.filter(applicant => applicant.id !== applicantId),
-  //     );
-
-  //     // setting new array of applicants with changed applicant
-  //     setApplicants(prevApplicants => {
-  //       // console.log(prevApplicants)
-  //       return [...prevApplicants, applicant];
-  //     });
-  //   }
-
-  //   // temporary
-  //   // if (applicantForApproval) {
-  //   //   applicantForApproval.status = 'approved';
-  //   // }
-  //   // setApplicants(prevApplicants => {
-  //   //   // console.log(prevApplicants)
-  //   //   return [...prevApplicants];
-  //   // });
-  // };
-
-  const getVisibleApplicants = () => {
-    return applicants.filter(applicant => applicant.name.toLowerCase());
-  };
-
-  const visibleApplicants = getVisibleApplicants();
 
   return (
     <>
       <Modal active={modalActive} setActive={setModalActive}>
-        <ApplicantForm 
-        // onSubmit={addApplicant} 
-        setActive={setModalActive} />
+        <ApplicantForm setActive={setModalActive} />
       </Modal>
-      <ApplicationList
-        applicants={visibleApplicants}
-        // onApplicantDelete={deleteApplicant}
-        setActive={setModalActive}
-        // onApplicantToInterview={makeApplicantAppointment}
-      />
-      <InterviewList
-        applicants={visibleApplicants}
-        // onApplicantDelete={deleteApplicant}
-        // onApproveApplicant={approveApplicant}
-      />
+      <ApplicationList applicants={applicants} setActive={setModalActive} />
+      <InterviewList applicants={applicants} />
 
-      <ApprovedList
-        applicants={visibleApplicants}
-        // onApplicantDelete={deleteApplicant}
-      />
+      <ApprovedList applicants={applicants} />
     </>
   );
 };
--- src/components/Dashboard/InterviewList/InterviewList.tsx
+++ src/components/Dashboard/InterviewList/InterviewList.tsx
@@ -2,19 +2,13 @@ import React from 'react';
 import ApplicantCard from '../../ApplicantCard/ApplicantCard';
 import s from './InterviewList.module.scss';
 
-import IApplicant from '../../../interfaces/IApplicant.inteface'
+import IApplicant from '../../../interfaces/IApplicant.inteface';
 
 interface Props {
   applicants: IApplicant[];
-  // onApplicantDelete: (applicantId: string) => void;
-  // onApproveApplicant: (applicantId: string) => void;
 }
 
-const InterviewList = ({
-  applicants,
-  // onApplicantDelete,
-  // onApproveApplicant,
-}: Props) => {
+const InterviewList = ({ applicants }: Props) => {
   return (
     <>
       <div className={s.list}>
@@ -22,23 +16,12 @@ const InterviewList = ({
 
         <ul>
           {applicants.map(applicant => {
-            // const { id, name, number, desiredPosition, status } = applicant;
             const { id, status } = applicant;
 
             return (
               <div className={s.card} key={id}>
                 {status === 'interview' && (
-                  <ApplicantCard
-                    // applicantName={name}
-                    // applicantNumber={number}
-                    // applicantDesiredPosition={desiredPosition}
-                    // onClickRemove={() => onApplicantDelete(id)}
-                    // onClickToApprove={() => onApproveApplicant(id)}
-                    applicantsData={applicant}
-                    // onClickRemove={onApplicantDelete}
-                    // onClickToApprove={onApproveApplicant}
-                    // applicantStatus={status}
-                  />
+                  <ApplicantCard applicantsData={applicant} />
                 )}
               </div>
             );

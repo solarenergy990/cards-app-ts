180bb44f3bd4016bc209bf5f4a93cd02b7028d0d
Eugen Soliar <eugen.soliar@exxeta.com>
2022-07-06 14:22:10 +0200

    erases useless comments in ApprovedList component

--- src/components/Dashboard/ApprovedList/ApprovedList.tsx
+++ src/components/Dashboard/ApprovedList/ApprovedList.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import ApplicantCard from '../../ApplicantCard/ApplicantCard';
 import s from './ApprovedList.module.scss';
 
-import IApplicant from '../../../interfaces/IApplicant.inteface'
+import IApplicant from '../../../interfaces/IApplicant.inteface';
 
 type Props = {
   applicants: IApplicant[];
-  // onApplicantDelete: (applicantId: string) => void;
-}
+};
 
-const ApprovedList = ({ applicants, 
-  // onApplicantDelete
- }: Props) => {
+const ApprovedList = ({ applicants }: Props) => {
   return (
     <>
       <div className={s.list}>
@@ -19,20 +16,12 @@ const ApprovedList = ({ applicants,
 
         <ul>
           {applicants.map(applicant => {
-            // const { id, name, number, desiredPosition, status } = applicant;
             const { id, status } = applicant;
 
             return (
               <div className={s.card} key={id}>
                 {status === 'approved' && (
-                  <ApplicantCard
-                  applicantsData={applicant}
-                    // applicantName={name}
-                    // applicantNumber={number}
-                    // applicantDesiredPosition={desiredPosition}
-                    // onClickRemove={onApplicantDelete}
-                    // applicantStatus={status}
-                  />
+                  <ApplicantCard applicantsData={applicant} />
                 )}
               </div>
             );

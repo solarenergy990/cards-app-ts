a782d56e310bc4b3743f01b1970daeb5db31988d
Eugen Soliar <eugen.soliar@exxeta.com>
2022-07-06 14:25:55 +0200

    erases useless comments from App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,16 +2,10 @@
 import Container from '../Container/Container';
 import Dashboard from '../Dashboard/Dashboard';
 
-// import StatusContext from '../../context/context';
-
 const App = () => {
-  // const [status, setStatus] = useState('');
-  
   return (
     <Container>
-      {/* <StatusContext.Provider value={{ status, setStatus }}> */}
-        <Dashboard />
-      {/* </StatusContext.Provider> */}
+      <Dashboard />
     </Container>
   );
 };

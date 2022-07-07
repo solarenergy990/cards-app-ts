2d84d5b18b203f44bd94739bc7d9b6413e41230f
Eugen Soliar <eugen.soliar@exxeta.com>
2022-07-07 09:03:01 +0200

    fixes button wrapper in Applicant component & erases useless comments

--- src/index.tsx
+++ src/index.tsx
@@ -8,15 +8,12 @@ import App from './components/App/App';
 
 import { Provider } from 'react-redux';
 
-import store from './redux/store'
-
-
-console.log(store.getState())
+import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),

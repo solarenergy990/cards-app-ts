7a8fa58da91d57f05d1611123ffa0cba438e5944
Eugen Soliar <eugen.soliar@exxeta.com>
2022-08-24 13:00:18 +0200

    adds additional layout to the ApplicationList component to mace it look nicer

--- src/components/Dashboard/ListContainer/ListContainer.tsx
+++ src/components/Dashboard/ListContainer/ListContainer.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
+
+import Card from 'react-bootstrap/Card';
 import s from './ListContainer.module.scss';
 
 type Props = {
   children: JSX.Element[] | JSX.Element;
-  title: string
+  title: string;
 };
 
 const ListContainer = ({ children, title }: Props) => {
   return (
-    <div className={s.list}>
-      <h2 className={s.title}>{title}</h2>
-      {children}
-    </div>
+    <>
+      <div className={s.list}>
+        <Card className="text-center">
+          <Card.Header>{title}</Card.Header>
+          <Card.Body className={s['card-body']}>{children}</Card.Body>
+        </Card>
+      </div>
+    </>
   );
 };
 
-export default ListContainer
+export default ListContainer;

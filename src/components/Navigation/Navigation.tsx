ec733ba0995f0f01bf07235897e897050066054a
Eugen Soliar <eugen.soliar@exxeta.com>
2022-08-24 14:36:12 +0200

    changes nav links

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
@@ -12,8 +13,14 @@ const Navigation = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="home">Home</Nav.Link>
-            <Nav.Link href="cards">Cards</Nav.Link>
+            <Link to='home'>
+              Home
+            </Link>
+            <Link to='cards'>
+              Cards
+            </Link>
+            {/* <Nav.Link href="home">Home</Nav.Link> */}
+            {/* <Nav.Link href="cards">Cards</Nav.Link> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
